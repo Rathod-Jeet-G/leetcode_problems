c37cfc4d9b66dbcf37d2d1bd4972e60a02c4951b
Rathod Jeet <86106611+Rathod-Jeet-G@users.noreply.github.com>
2024-01-18 10:59:35 +0530

    169.majority element.py

--- 169.majority element.py
+++ 169.majority element.py
@@ -1,3 +1,4 @@
+"""
 Given an array nums of size n, return the majority element.
 
 The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
@@ -58,7 +59,9 @@ class Solution {
         return val;
     }
 }
+
  -----------solution in python------------------------------------
+ """
  def majorityElement(self, nums: List[int]) -> int:
         count = 0
         val = 0
