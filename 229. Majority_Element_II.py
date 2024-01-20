75bd994d35ecbe46a47bbf7f99f688669a5195fe
Rathod Jeet <86106611+Rathod-Jeet-G@users.noreply.github.com>
2024-01-20 12:18:31 +0530

    229. Majority_Element_II.py

---
+++ 229. Majority_Element_II.py
@@ -0,0 +1,15 @@
+"""
+----------------------------------companies-------------------------------------------
+-> 0 - 6 months
+      Amazon 2
+      Adobe 2
+-> 6 months - 1 year
+      Apple 4
+      Google 3
+-> 1 year - 2 years
+      Microsoft 4
+      Bloomberg 4
+      Facebook 2
+      Rubrik 2
+      Zenefits
+"""
